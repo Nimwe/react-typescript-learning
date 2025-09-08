a43ba42b219097b46d23e0155c322ae2029a3c55
Nimwe <floallheilly@gmail.com>
2025-09-08 12:19:36 +0200

    DomModification

--- src/pages/DomModification/DomModificationPage.tsx
+++ src/pages/DomModification/DomModificationPage.tsx
@@ -5,7 +5,7 @@ import styles from "./DomModificationPage.module.css";
  * Ce composant va vous permettre de vous entraîner à la modification dynamique du DOM.
  * 
  * L'objectif est de :
- * 1. implémenter de boutons qui permettent de changer la couleur d'un titre
+ * 1. implémenter des boutons qui permettent de changer la couleur d'un titre
  * 2. implémenter un bouton qui permet de changer le style d'un paragraphe
  * 
  * Pour faire référence à un élément du DOM dans un code React vous pourrez utiliser le hook "useRef"
@@ -19,10 +19,13 @@ import styles from "./DomModificationPage.module.css";
  */
 const DomModificationPage = () => {
     // références pour les éléments du DOM
-    // référence vers le titre
+
+    // Référence vers le titre 
     const titleRef = useRef<HTMLHeadingElement>(null);
-    // TODO référence vers le paragraphe
-    // Quel est le type "TypeScript" a utiliser ?
+
+    // Référence vers le paragraphe
+    const paragraphRef = useRef<HTMLParagraphElement>(null);
+    // Quel est le type "TypeScript" a utiliser ? => <p>
 
     /**
      * Change la couleur du titre
@@ -33,12 +36,10 @@ const DomModificationPage = () => {
         let title = titleRef.current;
         // vérification : le titre est-il bien présent ?
         if (title != null) {
-            // TODO changement de la couleur
-            // il est possible d'accéder au style d'un élément HTML en faisant :
-            // title.style
-            // ceci provient du JS.
+            // Changement de la couleur
+            // il est possible d'accéder au style d'un élément HTML en faisant : title.style. Ceci provient du JS.
             // plus d'information sur la modification de la couleur en JS : https://www.w3schools.com/jsref/prop_style_color.asp
-            
+            title.style.color = color; // Changement réel de la couleur
         }
     };
 
@@ -53,23 +54,25 @@ const DomModificationPage = () => {
      */
     const toggleParagraphStyle = () => {
         // tentative de récupération de l'élément HTML paragraphe
-        //let paragraph = ?????;
+        const paragraph = paragraphRef.current;
 
-        //if (paragraph != null) {
+        if (paragraph != null) {
             // il est possible d'activer ou désactiver une classe en uilisant la méthode JS "toggle"
             // exemple d'utilisation : https://www.w3schools.com/howto/howto_js_toggle_class.asp
-
             // vérification si classe "highlighted" présente
-            //const isHighlighted = paragraph.classList.contains(styles.highlighted);
+            const isHighlighted = paragraph.classList.contains(styles.highlighted);
 
-            //if (isHighlighted) { // si c'est bien "highlighted"
+            if (isHighlighted) { // si c'est bien "highlighted"
                 // TODO insérer un toggle pour passer en normal
-                // paragraph.classList.toggle( ????????? )
-            //} else {
+                paragraph.classList.toggle(styles.highlighted); // retire highlighted
+                paragraph.classList.add(styles.normal); // ajoute normal
+            } else {
                 // TODO insérer un toggle pour passer en highlighted
-
-           // }
-       // }
+                paragraph.classList.toggle(styles.highlighted); // ajoute highlighted
+                paragraph.classList.remove(styles.normal); // retire normal
+            }
+        }
+        //// Penser à "attacher" la ref à l'element concerné => <p ref={paragraphRef} className={styles.paragraph + ' ' + styles.normal}></p> 
     };
 
     return (
@@ -97,7 +100,7 @@ const DomModificationPage = () => {
                     Vert
                 </button>
             </div>
-            <p className={styles.paragraph + ' ' + styles.normal}>
+            <p ref={paragraphRef} className={styles.paragraph + ' ' + styles.normal}> // Penser à "attacher" la ref à l'element concerné
                 Paragraphe avec style modifiable
             </p>
             <button
